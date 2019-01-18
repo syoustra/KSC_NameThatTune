2712f5ecef434ce9f439eef1d9814715d943c159
syoustra <steph.you07@gmail.com>
2019-01-18 03:42:20 -0500

    Add event listener to hear player guesses

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ function shuffle(a) {
     return a;
 }
 
+//Select the right and wrong songs for this question
 shuffle(songTitles);
 
 var selectedSong = songTitles.pop();
@@ -37,10 +38,25 @@ var songChoices = wrongSongs.push(selectedSong);
 songChoices = shuffle(wrongSongs);
 console.log("Options Seen By Player: " + songChoices.toString());
 
-
+//Display the song options
 document.getElementById("playedSong").innerHTML = "The song to be played is " + selectedSong;
 
 document.getElementById("choice1").innerHTML = "1. " + wrongSongs[0];
 document.getElementById("choice2").innerHTML = "2. " + wrongSongs[1];
 document.getElementById("choice3").innerHTML = "3. " + wrongSongs[2];
 document.getElementById("choice4").innerHTML = "4. " + wrongSongs[3];
+
+//Listen for player guesses
+window.addEventListener("keydown", checkKeyPressed, false);
+ 
+function checkKeyPressed(e) {
+    if (e.keyCode == "49") {
+        alert("Option 1 is selected");
+    } else if (e.keyCode == "50") {
+        alert("Option 2 is selected");
+    } else if (e.keyCode == "51") {
+        alert("Option 3 is selected");
+    } else if (e.keyCode == "52") {
+        alert("Option 4 is selected");
+    }
+}
