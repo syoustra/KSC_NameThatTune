4e330f571cdcb91de7b75d741000f03ba25eb73c
syoustra <steph.you07@gmail.com>
2019-01-18 04:29:03 -0500

    Checks to see if playerGuess is correct

--- app.js
+++ app.js
@@ -47,16 +47,55 @@ document.getElementById("choice3").innerHTML = "3. " + wrongSongs[2];
 document.getElementById("choice4").innerHTML = "4. " + wrongSongs[3];
 
 //Listen for player guesses
-window.addEventListener("keydown", checkKeyPressed, false);
+// window.addEventListener("keydown", checkKeyPressed);
  
-function checkKeyPressed(e) {
-    if (e.keyCode == "49") {
-        alert("Option 1 is selected");
-    } else if (e.keyCode == "50") {
-        alert("Option 2 is selected");
-    } else if (e.keyCode == "51") {
-        alert("Option 3 is selected");
-    } else if (e.keyCode == "52") {
-        alert("Option 4 is selected");
+// function checkKeyPressed(e) {
+//     if (e.keyCode == "49") {
+//         alert("Option 1 is selected");
+//     } else if (e.keyCode == "50") {
+//         alert("Option 2 is selected");
+//     } else if (e.keyCode == "51") {
+//         alert("Option 3 is selected");
+//     } else if (e.keyCode == "52") {
+//         alert("Option 4 is selected");
+//     }
+// }
+
+var playerGuess;
+// function checkGuess(playerGuess);
+window.onkeydown = function(e) {
+    switch (e.keyCode) {
+        case 49:
+            alert('1');
+            playerGuess = 1;
+            checkGuess(1);
+            break;
+        case 50:
+            alert('2');
+            playerGuess = 2;
+            checkGuess(2);
+            break;
+        case 51:
+            alert('3');
+            playerGuess = 3;
+            checkGuess(3);
+            break;
+        case 52:
+            alert('4');
+            playerGuess = 4;
+            checkGuess(4);
+            break;
     }
+  console.log("playerGuess = " + playerGuess);
+};
+
+function checkGuess() {
+  console.log(playerGuess);
+  var guessedSong = songChoices[playerGuess-1];
+  console.log(guessedSong);
+  if (guessedSong == selectedSong) {
+    alert("YOU WIN!!!!!");
+  } else {
+    alert("*sad trombone*");
+  }
 }
