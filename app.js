5a6f430c95967609526fa07e1771b67747145ee6
syoustra <steph.you07@gmail.com>
2019-01-24 22:35:15 -0500

    Verified removal of correct songs from array

--- app.js
+++ app.js
@@ -27,123 +27,129 @@ function shuffle(a) {
   return a;
 }
 
-//Select the right and wrong songs for this question
-shuffle(songTitles);
+function nextQuestion() {
+  //Select the right and wrong songs for this question
+  shuffle(songTitles);
 
-var selectedSong = songTitles.pop();
-var wrongSongs = songTitles.slice(0,3);
-console.log("Correct Song: " + selectedSong);
-console.log("Wrong Choices: " + wrongSongs.toString());
-var songChoices = wrongSongs.push(selectedSong);
-songChoices = shuffle(wrongSongs);
-console.log("Options Seen By Player: " + songChoices.toString());
+  var selectedSong = songTitles.pop();
+  var wrongSongs = songTitles.slice(0,3);
+  console.log("Correct Song: " + selectedSong);
+  console.log("Wrong Choices: " + wrongSongs.toString());
+  var songChoices = wrongSongs.push(selectedSong);
+  songChoices = shuffle(wrongSongs);
+  console.log("Options Seen By Player: " + songChoices.toString());
+  console.log("Current array length: " + songTitles.length);
 
-var songURL;
-function getSongURL(selectedSong) {
-  switch (selectedSong) {
-    case "Mario":
+  var songURL;
+  function getSongURL(selectedSong) {
+    switch (selectedSong) {
+      case "Mario":
       songURL = "ListentoGirlPlaysSuperMarioThemeSongonAncientChineseInstrument.mp3";
       break;
-    case "Yoshi":
+      case "Yoshi":
       songURL = "4-10-OverworldBGM.mp3";
       break;
-    case "Zelda":
+      case "Zelda":
       songURL = "01TitleTheme.mp3";
       break;
-    case "Rick Rollin":
+      case "Rick Rollin":
       songURL = "NeverGonnaGiveYouUp.mp3";
       break;
-    case "Ernie":
+      case "Ernie":
       songURL = "idontwanttoliveonthemoon.mp3";
       break;
-    case "Alligator King":
+      case "Alligator King":
       songURL = "alligator_king.mp3";
       break;
-    case "Simpsons":
+      case "Simpsons":
       songURL = "TheSimpsons.mp3";
       break;
-    case "Scooter":
+      case "Scooter":
       songURL = "Muppets-1993-MuppetHits-08-SimonSmithandtheAmazingDancingBear.mp3";
       break; 
-  }
-}
-
-getSongURL(selectedSong);
-// alert(songURL);
-
-//Display the song options
-document.getElementById("playedSong").innerHTML = "The song to be played is " + selectedSong + " with file located at " + songURL;
-
-document.getElementById("choice1").innerHTML = "1. " + wrongSongs[0];
-document.getElementById("choice2").innerHTML = "2. " + wrongSongs[1];
-document.getElementById("choice3").innerHTML = "3. " + wrongSongs[2];
-document.getElementById("choice4").innerHTML = "4. " + wrongSongs[3];
-
-//Listen for player guesses
-// window.addEventListener("keydown", checkKeyPressed);
-
-// function checkKeyPressed(e) {
-//     if (e.keyCode == "49") {
-//         alert("Option 1 is selected");
-//     } else if (e.keyCode == "50") {
-//         alert("Option 2 is selected");
-//     } else if (e.keyCode == "51") {
-//         alert("Option 3 is selected");
-//     } else if (e.keyCode == "52") {
-//         alert("Option 4 is selected");
-//     }
-// }
-
-
-///////////////////NEATEN EVENTLISTENER//////////////////////////////////
-var playerGuess;
-// function checkGuess(playerGuess);
-function listenForKeypress() {
-  window.onkeydown = function(e) {
-    switch (e.keyCode) {
-      case 49:
-      alert('1');
-      playerGuess = 1;
-      checkGuess(1);
-      break;
-      case 50:
-      alert('2');
-      playerGuess = 2;
-      checkGuess(2);
-      break;
-      case 51:
-      alert('3');
-      playerGuess = 3;
-      checkGuess(3);
-      break;
-      case 52:
-      alert('4');
-      playerGuess = 4;
-      checkGuess(4);
-      break;
     }
-    console.log("playerGuess = " + playerGuess);
-  };
-}
+  }
 
-listenForKeypress();
+  getSongURL(selectedSong);
+  // alert(songURL);
 
-function checkGuess() {
-  console.log(playerGuess);
-  var guessedSong = songChoices[playerGuess-1];
-  console.log(guessedSong);
-  if (guessedSong == selectedSong) {
-    alert("YOU WIN!!!!!");
-    document.getElementById("choice1").innerHTML = "1. By the Power";
-    document.getElementById("choice2").innerHTML = "2. of Greyskull...";
-    document.getElementById("choice3").innerHTML = "3. You are the";
-    document.getElementById("choice4").innerHTML = "4. Winner!!!!!";
+  //Display the song options
+  document.getElementById("playedSong").innerHTML = "The song to be played is " + selectedSong + " with file located at " + songURL;
 
-  } else {
-    alert("*sad trombone*");
-    document.getElementById("choice1").innerHTML = "1. I";
-    document.getElementById("choice2").innerHTML = "2. Pity";
-    document.getElementById("choice3").innerHTML = "3. The";
-    document.getElementById("choice4").innerHTML = "4. Fool....";
+  document.getElementById("choice1").innerHTML = "1. " + wrongSongs[0];
+  document.getElementById("choice2").innerHTML = "2. " + wrongSongs[1];
+  document.getElementById("choice3").innerHTML = "3. " + wrongSongs[2];
+  document.getElementById("choice4").innerHTML = "4. " + wrongSongs[3];
+
+  //Listen for player guesses
+  // window.addEventListener("keydown", checkKeyPressed);
+
+  // function checkKeyPressed(e) {
+  //     if (e.keyCode == "49") {
+  //         alert("Option 1 is selected");
+  //     } else if (e.keyCode == "50") {
+  //         alert("Option 2 is selected");
+  //     } else if (e.keyCode == "51") {
+  //         alert("Option 3 is selected");
+  //     } else if (e.keyCode == "52") {
+  //         alert("Option 4 is selected");
+  //     }
+  // }
+
+
+  ///////////////////NEATEN EVENTLISTENER//////////////////////////////////
+  var playerGuess;
+  // function checkGuess(playerGuess);
+  function listenForKeypress() {
+    window.onkeydown = function(e) {
+      switch (e.keyCode) {
+        case 49:
+        alert('1');
+        playerGuess = 1;
+        checkGuess(1);
+        break;
+        case 50:
+        alert('2');
+        playerGuess = 2;
+        checkGuess(2);
+        break;
+        case 51:
+        alert('3');
+        playerGuess = 3;
+        checkGuess(3);
+        break;
+        case 52:
+        alert('4');
+        playerGuess = 4;
+        checkGuess(4);
+        break;
+      }
+      console.log("playerGuess = " + playerGuess);
+    };
+  }
+
+  listenForKeypress();
+
+  function checkGuess() {
+    console.log(playerGuess);
+    var guessedSong = songChoices[playerGuess-1];
+    console.log(guessedSong);
+    if (guessedSong == selectedSong) {
+      alert("YOU WIN!!!!!");
+      document.getElementById("choice1").innerHTML = "1. By the Power";
+      document.getElementById("choice2").innerHTML = "2. of Greyskull...";
+      document.getElementById("choice3").innerHTML = "3. You are the";
+      document.getElementById("choice4").innerHTML = "4. Winner!!!!!";
+
+    } else {
+      alert("*sad trombone*");
+      document.getElementById("choice1").innerHTML = "1. I";
+      document.getElementById("choice2").innerHTML = "2. Pity";
+      document.getElementById("choice3").innerHTML = "3. The";
+      document.getElementById("choice4").innerHTML = "4. Fool....";
+    }
+    nextQuestion();
   }
 }
+
+nextQuestion();
